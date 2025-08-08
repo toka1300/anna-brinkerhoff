48841024dd2afc4df25f8662512214c3adf07d1a
toka1300 <caseytokarchuk@gmail.com>
2025-08-08 09:19:42 -0400

    fine-tuning

--- src/assets/js/faq.js
+++ src/assets/js/faq.js
@@ -1,7 +1,11 @@
 const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
-        for (const item of faqItems) {
-            const onClick = () => {
-            item.classList.toggle('active')
-        }
-        item.addEventListener('click', onClick)
-        }
+
+for (const item of faqItems) {
+    const onClick = (e) => {
+        if (e.target.closest('a')) return;
+
+        item.classList.toggle('active');
+    }
+
+    item.addEventListener('click', onClick);
+}
